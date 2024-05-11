049d29a43acab7e256ab8701adc017bbc296c290
kushalshrestha <kushalshr@gmail.com>
2024-05-11 15:25:46 -0700

    chore: project init

---
+++ README.md
@@ -0,0 +1,6 @@
+# kushal-portfolio
+
+This is a personal website of Kushal Shrestha in React with Vite.
+
+
+
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Kushal Shrestha</h1>
+      <h2>Software Engineer</h2>
+    </>
+  )
+}
+
+export default App
